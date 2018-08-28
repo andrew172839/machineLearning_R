8f3c04d6a626375e076a61d7a2e75dbe7fb173dd
andrew789 <yushao.cheng@gmail.com>
2018-08-28 11:24:29 +0800

    .

--- heirarchicalClustering.R
+++ heirarchicalClustering.R
@@ -1,33 +1,29 @@
-#hierarchical clustering
+# hierarchical clustering
 train = read.csv("train.csv")
 train = train[, 1: 41]
 X = as.matrix(train[, -1]);
 dim(X)
 unique(colnames(X))
 
-#complete linakge - euclidean distance
+# complete linakge, euclidean distance
 cols = as.numeric(as.factor(colnames(X)))
-Dmat = dist(t(X))
-com.hclust = hclust(Dmat, method = "complete")
-plot(com.hclust, cex = .7, main = "Complete Linkage")
+dmat = dist(t(X))
+complete.hclust = hclust(dmat, method = "complete")
+plot(complete.hclust, main = "complete linkage")
 
-#single linakge
-dev.new()
-sing.hclust = hclust(Dmat, method = "single")
-plot(sing.hclust, cex = .7, main = "Single Linkage")
+# single linakge
+single.hclust = hclust(dmat, method = 'single')
+plot(single.hclust, main = 'single linkage')
 
-#average linakge
-dev.new()
-ave.hclust = hclust(Dmat, method = "average")
-plot(ave.hclust, cex = .7, main = "Average Linkage")
+# average linakge
+average.hclust = hclust(dmat, method = "average")
+plot(average.hclust, main = "average linkage")
 
-#ward's linakge
-dev.new()
-ward.hclust = hclust(Dmat, method = "ward.D")
-plot(ward.hclust, cex = .7, main = "Ward's Linkage")
+# ward's linakge
+ward.hclust = hclust(dmat, method = "ward.D")
+plot(ward.hclust, main = "ward's linkage")
 
-#complete linkage with different distances
-dev.new()
-Dmat = dist(t(X), method = "manhattan")
-com.hclust = hclust(Dmat, method = "complete")
-plot(com.hclust, cex = .7, main = "Complete Linkage - L1 Dist")
+# complete linkage with different distances
+dmat = dist(t(X), method = "manhattan")
+complete.hclust = hclust(dmat, method = "complete")
+plot(complete.hclust, main = "complete linkage - l1")
