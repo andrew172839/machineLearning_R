1cd980cafc5afc0102e798d31233ae31deed376e
andrew789 <yushao.cheng@gmail.com>
2018-08-28 13:53:37 +0800

    .

--- kMeans.R
+++ kMeans.R
@@ -1,53 +1,50 @@
-#k-means
+# k means
 n = 300
 mu1 = c(3, 3)
 mu2 = c(7, 4)
 mu3 = c(6, 5.5) 
-Sig = matrix(c(1, .5, .5, 1), 2, 2)
+sigma = matrix(c(1, .5, .5, 1), 2, 2)
 x1 = t(matrix(mu1, 2, n / 3)) + matrix(rnorm(n), n / 3, 2)
 xx = matrix(rnorm(n * 2 / 3), n / 3, 2)
-x2 = t(matrix(mu2, 2, n / 3)) + xx %*% chol(Sig)
+x2 = t(matrix(mu2, 2, n / 3)) + xx %*% chol(sigma)
 xx = matrix(rnorm(n * 2 / 3), n / 3, 2)
-x3 = t(matrix(mu3, 2, n / 3)) + xx %*% chol(Sig)
+x3 = t(matrix(mu3, 2, n / 3)) + xx %*% chol(sigma)
 x = rbind(x1, x2, x3)
 Y = c(rep(1, n / 3), rep(2, n / 3), rep(3, n / 3))
 y = factor(Y)
+plot(x[, 1], x[, 2], col = as.numeric(y))
 
-plot(x[, 1], x[, 2], col = as.numeric(y), pch = 16)
-
-#try changing k
 k = 3
 km = kmeans(x, centers = k)
-plot(x[, 1], x[, 2], col = km$cluster, pch = 16)
-cens = km$centers
-points(cens[, 1], cens[, 2], col = 1: k, pch = 16, cex = 3)
+plot(x[, 1], x[, 2], col = km$cluster)
+centers = km$centers
+points(centers[, 1], centers[, 2], col = 1: k, cex = 3)
 
-#code to understand k-means algorithm
 require("animation")
-mv.kmeans = function(x, k, cens = NULL)
+kmeans = function(x, k, centers = NULL)
 {
 	n = nrow(x)
-	if (is.null(cens))
+	if (is.null(centers))
 	{
-		cens = x[sample(1: n, k),]
+		centers = x[sample(1: n, k),]
 	}
-	plot(x[, 1], x[, 2], pch = 16)
-	points(cens[, 1], cens[, 2], col = 1: k, pch = 16, cex = 3)
+	plot(x[, 1], x[, 2])
+	points(centers[, 1], centers[, 2], col = 1: k, cex = 3)
 	thr = 1e-6
-	ind = 1
+	index = 1
 	iter = 1
-	while (ind > thr)
+	while (index > thr)
 	{
-		oldcen = cens
-		km = kmeans(x, centers = cens, iter.max = 1, nstart = 1, algorithm = "MacQueen")
-		plot(x[, 1], x[, 2], col = km$cluster, pch = 16)
-		points(cens[, 1], cens[, 2], col = 1: k, pch = 16, cex = 3)
-		cens = km$centers
-		print(cens)
-		plot(x[, 1], x[, 2], col = km$cluster, pch = 16)
-		points(cens[, 1], cens[, 2], col = 1: k, pch = 16, cex = 3)
-		ind = sum(diag((oldcen - cens) %*% t(oldcen - cens)))
-		print(ind)
+		oldcenter = centers
+		km = kmeans(x, centers = centers, iter.max = 1, nstart = 1, algorithm = 'macqueen')
+		plot(x[, 1], x[, 2], col = km$cluster)
+		points(centers[, 1], centers[, 2], col = 1: k, cex = 3)
+		centers = km$centers
+		print(centers)
+		plot(x[, 1], x[, 2], col = km$cluster)
+		points(centers[, 1], centers[, 2], col = 1: k, cex = 3)
+		index = sum(diag((oldcenter - centers) %*% t(oldcenter - centers)))
+		print(index)
 	} 
 }
 
@@ -55,31 +52,29 @@ train = read.csv("train.csv")
 train = train[, 1: 41]
 X = as.matrix(train[, -1]);
 
-#apply k-means
-K = 9
-km = kmeans(t(X), centers = K)
+k = 9
+km = kmeans(t(X), centers = k)
 
-#pca - take svd to get solution
+# pca
 sv = svd(X)
 U = sv$u
 V = sv$v
 D = sv$d
 Z = X %*% V
 
-plot(Z[, 1], Z[, 2], col = km$cluster, type = "n")
-text(Z[, 1], Z[, 2], colnames(X), cex = .75, col = km$cluster)
-cens = km$centers
+plot(Z[, 1], Z[, 2], col = km$cluster)
+text(Z[, 1], Z[, 2], colnames(X), cex = 0.75, col = km$cluster)
+centers = km$centers
 
-#re-run and see if solution changes
-K = 9
-km = kmeans(t(X), centers = K)
-plot(Z[, 1], Z[, 2], col = km$cluster, type = "n")
-text(Z[, 1], Z[, 2], colnames(X), cex = .75, col = km$cluster)
-cens = km$centers
+# re-run
+k = 9
+km = kmeans(t(X), centers = k)
+plot(Z[, 1], Z[, 2], col = km$cluster)
+text(Z[, 1], Z[, 2], colnames(X), cex = 0.75, col = km$cluster)
+centers = km$centers
 
-#try different k
-K = 5
-km = kmeans(t(X), centers = K)
-plot(Z[, 1], Z[, 2], col = km$cluster, type = "n")
-text(Z[, 1], Z[, 2], colnames(X), cex = .75, col = km$cluster)
-cens = km$centers
+k = 5
+km = kmeans(t(X), centers = k)
+plot(Z[, 1], Z[, 2], col = km$cluster)
+text(Z[, 1], Z[, 2], colnames(X), cex = 0.75, col = km$cluster)
+centers = km$centers
