0950d8bbc7a0be709ff490eb26bc9cf1876c9db9
andrew789 <yushao.cheng@gmail.com>
2018-08-29 14:25:57 +0800

    .

--- logisticRegression.R
+++ logisticRegression.R
@@ -1,5 +1,4 @@
-#univariate logistic regression - simulated example
-#change coefficients to understand logistic function
+# univariate logistic regression, simulated example
 n = 250
 p = 1
 x = matrix(rnorm(n * p), n, p)
@@ -7,16 +6,16 @@ beta0 = 0
 beta = 1
 eps = matrix(rnorm(n), n, 1)
 probs = exp(beta0 + x * beta + eps) / (1 + exp(beta0 + x * beta + eps))
-Y = as.numeric(probs > .5)
+Y = as.numeric(probs > 0.5)
 
-#fit logistic model
+# logistic model
 fit = glm(Y ~ x, family = "binomial")
 summary(fit)
 plot(x, Y)
 xs = seq(min(x), max(x), l = 1000)
 pihat = exp(fit$coefficients[1] + xs * fit$coefficients[2]) / (1 + exp(fit$coefficients[1] + xs * fit$coefficients[2]))
 lines(xs, pihat)
-lines(c(min(x), max(x)), c(.5, .5), lty = 2)
+lines(c(min(x), max(x)), c(0.5, 0.5))
 
 train = read.csv("train.csv", header = FALSE)
 train = train[, 1: 41]
@@ -27,7 +26,7 @@ X = scale(X) / sqrt(n - 1)
 dat = data.frame(Y, X)
 sdat = data.frame(Y, dat$V2, dat$V3, dat$V4, dat$V5, dat$V6, dat$V7, dat$V8, dat$V9, dat$V10)
 
-fits = glm(Y~., data = sdat, family = "binomial", maxit = 50)
+fits = glm(Y~., data = sdat, family = "binomial")
 summary(fits)
 
 fit = glm(Y~dat.V2, data = sdat, family = "binomial")
@@ -35,11 +34,11 @@ summary(fit)
 plot(sdat$dat.V2, sdat$Y)
 pihat = exp(fit$coefficients[1] + xs * fit$coefficients[2]) / (1 + exp(fit$coefficients[1] + xs * fit$coefficients[2]))
 lines(xs, pihat)
-lines(c(min(xs), max(xs)), c(.5, .5), lty = 2)
+lines(c(min(xs), max(xs)), c(0.5, 0.5))
 
 fit = glm(Y~dat.V10, data = sdat, family = "binomial")
 summary(fit)
 plot(sdat$dat.V10, sdat$Y)
 pihat = exp(fit$coefficients[1] + xs * fit$coefficients[2]) / (1 + exp(fit$coefficients[1] + xs * fit$coefficients[2]))
 lines(xs, pihat)
-lines(c(min(xs), max(xs)), c(.5, .5), lty = 2)
+lines(c(min(xs), max(xs)), c(0.5, 0.5))
