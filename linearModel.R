dd7a93990c0dfdaaea2dd30983133f782b863664
andrew789 <yushao.cheng@gmail.com>
2018-08-29 11:33:12 +0800

    .

--- linearModel.R
+++ linearModel.R
@@ -1,28 +1,27 @@
 train = read.csv("train.csv")
 train = train[, 1: 41]
 
-#directly fit least squares
+# least squares
 Y = as.numeric(train[, 1])
-aX = cbind(rep(1, nrow(train)), as.matrix(train[, -1]))
+X = cbind(rep(1, nrow(train)), as.matrix(train[, -1]))
 
-#estimated coefficients
-betahat = solve(t(aX) %*% aX) %*% t(aX) %*% Y
+# coefficients
+betahat = solve(t(X) %*% X) %*% t(X) %*% Y
 
-#fitted data
-Yhat = aX %*% betahat
+# fitted data
+Yhat = X %*% betahat
 
-#hat matrix
-H = aX %*% solve(t(aX) %*% aX) %*% t(aX)
+# hat matrix
+H = X %*% solve(t(X) %*% X) %*% t(X)
 evH = eigen(H)
 evH$values[1: 10]
 
-#fit linear model
+# linear model
 fit = lm(data = train)
 summary(fit)
 
 cbind(betahat, t(t(fit$coefficients)))
 
-#diagnostic plots
 layout(matrix(c(1, 2, 3, 4), 2, 2))
 plot(fit)
 dev.off()
