ec30c6e123e17526abad7ad42b3efa129567a0df
andrew789 <yushao.cheng@gmail.com>
2018-08-31 17:12:28 +0800

    .

--- svd.R
+++ svd.R
@@ -1,20 +1,20 @@
-train = read.csv("train.csv")
+train = read.csv('train.csv')
 train = train[, 1: 41]
 X = as.matrix(train[, -1])
 
-#pca - take svd to get solution
+# pca
 svd = svd(X)
 U = svd$u
 V = svd$v
 D = svd$d
 Z = X %*% V
 
-#pc scatterplot
+# pcs
 par(mfrow = c(1, 1))
-plot(Z[, 2], Z[, 3], pch = 16)
+plot(Z[, 2], Z[, 3])
 pX = X %*% V[, 1: 25]
 
-#ica
-require("fastICA")
-K = 15
-icafit = fastICA(t(X), n.comp = K)
+# ica
+require('fastICA')
+k = 15
+icafit = fastICA(t(X), n.comp = k)
